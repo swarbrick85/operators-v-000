cfdc14e1af556d208a3d5caf882b04c856c25281
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-07 17:44:15 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -4,11 +4,12 @@ if speed < 40 || speed > 60
 else 
   return false
 end
+end
 
 
 
 def not_safe?(speed)
-	speed < 40 || speed > 60 ? true : false
+	(speed < 40 || speed > 60) ? true : false
 end
 	
 
